f7136c6c187fbd67af86699087e86b3772a6e270
Martsynyuk <terr83@gmail.com>
2016-08-15 08:23:04 +0300

    new dump

--- db.sql
+++ db.sql
@@ -16,23 +16,23 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`guestboard` /*!40100 DEFAULT CHARACTER 
 
 USE `guestboard`;
 
-/*Table structure for table `information` */
+/*Table structure for table `post` */
 
-DROP TABLE IF EXISTS `information`;
+DROP TABLE IF EXISTS `post`;
 
-CREATE TABLE `information` (
+CREATE TABLE `post` (
   `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(20) NOT NULL,
   `title` varchar(120) NOT NULL,
   `body` varchar(300) NOT NULL,
-  `lat` int(15) NOT NULL,
-  `lng` int(15) NOT NULL,
+  `lat` decimal(20,15) NOT NULL,
+  `lng` decimal(20,15) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
-/*Data for the table `information` */
+/*Data for the table `post` */
 
-insert  into `information`(`id`,`user_id`,`title`,`body`,`lat`,`lng`) values (3,17,'news','dfdsffds',115515,4845551),(4,17,'news','textdsfdfdsf',115515,4845551),(12,17,'syslic','As someone living with MS herself and who has a huge passion for dance, Trishna ',455435,435435),(13,17,'homyak','sdsadasd sadasd dasd asd dasds asd sddasd asdas d',32432432,32432432);
+insert  into `post`(`id`,`user_id`,`title`,`body`,`lat`,`lng`) values (13,17,'news','news create','51.761914853084540','33.390820312500000'),(14,18,'one','asds asdasd asd asdasd asdasd dasdas ','49.000000015455500','26.000415445000000'),(15,17,'topic','test topic !','51.013754657188215','29.575195312500000'),(16,17,'text','new text','50.401515322782366','33.310546875000000'),(17,17,'topic','new topic with text','50.035973672195496','27.465820312500000'),(18,17,'topic','test','48.893615361480194','29.509277343750000');
 
 /*Table structure for table `users` */
 
@@ -44,11 +44,11 @@ CREATE TABLE `users` (
   `email` varchar(30) DEFAULT NULL,
   `password` varchar(34) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`username`,`email`,`password`) values (17,'AntonM','terr83@gmail.com','7e737473ed73006755a0b3f881367f62');
+insert  into `users`(`id`,`username`,`email`,`password`) values (17,'AntonM','terr83@gmail.com','7e737473ed73006755a0b3f881367f62'),(18,'Genuen','tester@mai.ru','7e737473ed73006755a0b3f881367f62');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
