595beb05fb11894274c9a046551d8b2afd524406
Martsynyuk <terr83@gmail.com>
2016-08-29 12:52:06 +0300

    js

--- webroot/js/loadMap.js
+++ webroot/js/loadMap.js
@@ -1,29 +1,33 @@
 "use strict";
-
-if(document.readyState == 'complete') {
-	
-}
-	
-var Map = {
-	markers: [],
-	loadMap: new google.maps.Map(document.getElementById('map'), {
-				center: {lat: 49.007073, lng: 30.2600184},
-				zoom: 6
-			}),
-	addMarker: function (lat, lng) {
-				var marker = new google.maps.Marker({
-					position: {lat: lat, lng: lng},
-					map: Map.loadMap,
-			});	
-				Map.markers.push(marker);
-		},
-	clearMarker: function () {
-			if(Map.markers.length > 0) {
-				for(var i = 0; i < Map.markers.length; i++) {
-					Map.markers[i].setMap(null);
+console.log('1');
+define('map', ['googleMaps'], function(){
+	console.log('2');
+	if(document.readyState == 'complete') {
+			
+	}
+		
+	var Map = {
+		markers: [],
+		loadMap: new google.maps.Map(document.getElementById('map'), {
+					center: {lat: 49.007073, lng: 30.2600184},
+					zoom: 6
+				}),
+		addMarker: function (lat, lng) {
+					var marker = new google.maps.Marker({
+						position: {lat: lat, lng: lng},
+						map: Map.loadMap,
+				});	
+					Map.markers.push(marker);
+			},
+		clearMarker: function () {
+				if(Map.markers.length > 0) {
+					for(var i = 0; i < Map.markers.length; i++) {
+						Map.markers[i].setMap(null);
+					}
 				}
-			}
-		},
-}
+			},
+	}
+});
+
 	
 
