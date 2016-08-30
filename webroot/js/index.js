86b4a4091344fc0e428d743d5cfe99417605e40e
Martsynyuk <terr83@gmail.com>
2016-08-30 21:02:55 +0300

    js

--- webroot/js/index.js
+++ webroot/js/index.js
@@ -8,36 +8,41 @@ define(['./loadMap.js'], function(Map) {
 		},
 		
 		addMarkers: function() {
-			var content = null;
 			var points = [];
-			var windows = [];
-			var markers = [];
-			for(var i = 0; i < createAllMarkers.post.length; i++) {
-				content = '<div style="color: black;" class="wininfo">'+ 
-				    		'<div class="title">'+ createAllMarkers.post[i]['title'] +
-				    		'<div class="text">' + createAllMarkers.post[i]['body'] +
-				    		'</div>'+
-				    		'</div>'+
-							'</div>';
-				points.push({'lat': parseFloat(createAllMarkers.post[i]['lat']), 'lng': parseFloat(createAllMarkers.post[i]['lng'])});
-				windows.push(Map.infoWindows(content));			
-				markers.push(Map.addMarker(createAllMarkers.map, parseFloat(createAllMarkers.post[i]['lat']), parseFloat(createAllMarkers.post[i]['lng'])));
+			var markers = null;
+			for(var i = 0; i < createAllMarkers.post.length; i++) {	
+				points.push({'lat': parseFloat(createAllMarkers.post[i]['lat']), 'lng': parseFloat(createAllMarkers.post[i]['lng'])});			
+				markers = Map.addMarker(createAllMarkers.map, parseFloat(createAllMarkers.post[i]['lat']), parseFloat(createAllMarkers.post[i]['lng']));
+				createAllMarkers.eventsOnMarkers(markers, createAllMarkers.post[i]['title'], createAllMarkers.post[i]['body']);
 			}
 			createAllMarkers.centeringMap(points);
-			
-			markers[0].addListener('click', function() {
-				windows[0].open(createAllMarkers.map, markers[0]);
-			});
 		},
 		
 		centeringMap: function(points) {
 			var latlngbounds = new google.maps.LatLngBounds();
-			for ( var i=0; i < points.length; i++ ){
+			for ( var i = 0; i < points.length; i++ ){
 			     latlngbounds.extend(points[i]);
 			}
 			createAllMarkers.map.setCenter( latlngbounds.getCenter(), createAllMarkers.map.fitBounds(latlngbounds));
+		},
+		
+		eventsOnMarkers: function(markers, title, body) {
+		
+			var content = '<div style="color: black;" class="wininfo">'+ 
+    						'<div class="title">'+ title +
+    						'<div class="text">' + body +
+    						'</div>'+
+    						'</div>'+
+    						'</div>';
+		 
+			var infowindow = new google.maps.InfoWindow({
+			    content: content
+			});
+			
+			markers.addListener('click', function() {
+			    infowindow.open(markers.get('createAllMarkers.'), markers);
+			});
 		}
 	}
-	//return console.log(createAllMarkers.post);
 	return createAllMarkers.start();
 });
